6e30f0562813f3df4d7da8189ebaa5cf123656bb
aditya yadav <adityakumaryadav74@gmail.com>
2025-06-08 09:13:04 +0530

    edited Canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import dynamic from 'next/dynamic'; // Import dynamic to handle SSR
 import "@excalidraw/excalidraw/index.css";
 
-// Dynamically import Excalidraw to prevent SSR issues
 const Excalidraw = dynamic(() => import('@excalidraw/excalidraw').then(mod => mod.Excalidraw), {
-  ssr: false, // Disable SSR for this component
+  ssr: false,
 });
 
 const Canvas = () => {
